fc734e2138e3e361613952f2ae823081ac128475
smooto <melissasmoot@live.com>
2020-06-08 16:17:44 -0700

    Refactor parseRequest to handle more use cases

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,5 +2,10 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 describe('createResponse', () => {
-
+  it('returns plaintext \'hi\' on GET to \'\\\' ', () => {
+    return request(app)
+      .get('/')
+      .send('test body')
+      .then(res => console.log(res.text));
+  });
 });
