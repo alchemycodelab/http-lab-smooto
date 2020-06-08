3846e7a547ded1949d5ce74ab942524c879f0066
smooto <melissasmoot@live.com>
2020-06-08 16:43:07 -0700

    Add GET to color routes + tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,9 +2,24 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 describe('createResponse', () => {
-  it('returns plaintext \'hi\' on GET to \'\\\' ', () => {
+  it('returns plaintext \'hi\' on GET to \'/\' ', () => {
     return request(app)
       .get('/')
       .then(res => expect(res.text).toEqual('hi'));
+  });
+  it('returns h1 \'red\' on GET to \'/red\' ', () => {
+    return request(app)
+      .get('/red')
+      .then(res => expect(res.text).toEqual('<h1>red</h1>'));
+  });
+  it('returns h1 \'blue\' on GET to \'/blue\' ', () => {
+    return request(app)
+      .get('/blue')
+      .then(res => expect(res.text).toEqual('<h1>blue</h1>'));
+  });
+  it('returns h1 \'green\' on GET to \'/green\' ', () => {
+    return request(app)
+      .get('/green')
+      .then(res => expect(res.text).toEqual('<h1>green</h1>'));
   });
 });
