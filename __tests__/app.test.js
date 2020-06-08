a8f7e4352f38803187d404a2e1c538fc459f6e8c
smooto <melissasmoot@live.com>
2020-06-08 16:35:16 -0700

    Add GET to / and test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -5,7 +5,6 @@ describe('createResponse', () => {
   it('returns plaintext \'hi\' on GET to \'\\\' ', () => {
     return request(app)
       .get('/')
-      .send('test body')
-      .then(res => console.log(res.text));
+      .then(res => expect(res.text).toEqual('hi'));
   });
 });
